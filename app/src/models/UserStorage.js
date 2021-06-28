3c7a863b55451716b917fad7de02cb7d378dd578
DONGHAEY <dongheaey@gmail.com>
2021-06-28 21:25:59 +0900

    회원가입요청구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -2,9 +2,9 @@
 
 class UserStorage {
     static #users = { //#숨기겠다는 뜻 외부에서 인출 못함 ㅅㄱ
-        id : ["admin", "ODONG", "DONGHAEY"],
-        psword : ["1234", "1234", "123456"],
-        name : ["어디민", "오동헤", "동헤이"],
+        id : ["admin", "ODONG", "DONGHAEY", "adim"],
+        psword : ["1234", "1234", "123456", "1234"],
+        name : ["어디민", "오동헤", "동헤이", "에딤"],
     };
 
     static getUsers(...fields) { //제귀 호출사용 여부 O//
